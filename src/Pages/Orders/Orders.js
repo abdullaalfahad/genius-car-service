3d0cc2429309884d28b510e559454b195b97532c
abdullaalfahad <abdullaalfahad.cse@gmail.com>
2022-05-03 01:02:17 +0600

    orders added

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -1,10 +1,23 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Orders = () => {
-    const [orders, setOrders] = useState([])
+    const [user] = useAuthState(auth);
+    const email = user.email;
+    const [orders, setOrders] = useState([]);
+    useEffect(() => {
+        const getOrders = async () => {
+            const url = `http://localhost:5000/order?email=${email}`;
+            const { data } = await axios.get(url);
+            setOrders(data);
+        }
+        getOrders();
+    }, [user])
     return (
         <div className='container my-5'>
-            <h1 className='text-center'>Your Orders</h1>
+            <h1 className='text-center'>Your Orders {orders.length}</h1>
         </div>
     );
 };
